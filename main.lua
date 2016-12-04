e1baf2d25e27634fda1a804eec6ccff2c1b3cdda
zorggn <zorg@atw.hu>
2016-12-04 01:16:18 +0100

    Temporary effect rotation
    A badly written showcase.

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 local width, height
 local sounddata, source
+local timer, changeTime
 
 local wavescope
 
 function loadFile(file)
+	timer = 0.0
 	if type(file) == 'string' then
 		file = love.filesystem.newFile(file)
 	end
@@ -24,6 +26,7 @@ end
 
 function love.load()
 	width, height = love.graphics.getDimensions()
+	changeTime = 2.0 -- seconds
 	loadFile('vistestsong.ogg')
 end
 
@@ -32,6 +35,15 @@ function love.filedropped(file)
 end
 
 function love.update(dt)
+	timer = timer + dt
+	if timer >= changeTime then
+		wavescope.blockRate  = ({1,8,16,32,64,128,256,512,1024})[love.math.random(1,9)]
+		wavescope.blockSize  = ({1,8,16,32,64,128,256,512,1024})[love.math.random(1,9)]
+		wavescope.blockCount = ({1,8,16,32,64,128,256,512,1024})[love.math.random(1,9)]
+		wavescope.blockMode  = ({'signed','unsigned','mirrored'})[love.math.random(1,3)]
+		wavescope.renderMode = ({'scan','scroll','sweep'})[love.math.random(1,3)]
+		timer = 0.0
+	end
 	wavescope:update()
 end
 

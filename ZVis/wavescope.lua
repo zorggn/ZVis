b410a5cc579064cc81a33f42513a13ed1283b8c2
zorggn <zorg@atw.hu>
2017-01-05 11:37:11 +0100

    Finalize window functions

--- ZVis/wavescope.lua
+++ ZVis/wavescope.lua
@@ -5,22 +5,28 @@
 local visualizer = {}
 
 -- Getters
-visualizer.getWindowPosition = function(vis) return vis.left, vis.top end
-visualizer.getWindowDimensions = function(vis) return vis.width, vis.height end
+visualizer.getWindowPosition   = function(vis)
+	return vis.window.left, vis.window.top
+	end
+visualizer.getWindowDimensions = function(vis)
+	return vis.window.width, vis.window.height
+	end
+
 
 -- Setters
-visualizer.setWindowPosition = function(vis, left, top)
-	assert(type(left) == 'number' or type(left) == 'nil', "Visualizer's left position must be a number.")
-	assert(type(top)  == 'number' or type(top)  == 'nil', "Visualizer's top position must be a number.")
-	vis.left = left or vis.left
-	vis.top  = top  or vis.top
-end
+visualizer.setWindowPosition   = function(vis, left, top)
+	assert(type(left) == 'number' or type(left) == 'nil', "Left must be a number or nil.")
+	assert(type(top)  == 'number' or type(top)  == 'nil', "Top must be a number or nil.")
+	vis.window.left = left or vis.window.left
+	vis.window.top  = top  or vis.window.top
+	end
 visualizer.setWindowDimensions = function(vis, width, height)
-	assert(type(width)  == 'number' or type(width)  == 'nil', "Visualizer's width must be a number.")
-	assert(type(height) == 'number' or type(height) == 'nil', "Visualizer's height must be a number.")
-	vis.width  = width  or vis.width
-	vis.height = height or vis.height
-end
+	assert(type(width)  == 'number' or type(width)  == 'nil', "Width must be a number or nil.")
+	assert(type(height) == 'number' or type(height) == 'nil', "Height must be a number or nil.")
+	vis.window.width  = width  or vis.window.width
+	vis.window.height = height or vis.window.height
+	end
+
 
 -- Metatable
 local mtVisualizer = {__index = visualizer}
@@ -30,6 +36,7 @@ local new = function(left, top, width, height, properities)
 	local vis = setmetatable({}, mtVisualizer)
 
 	-- Set window properities. (Same for all visualizer types.)
+	vis.window = {}
 	vis:setWindowPosition(left, top)
 	vis:setWindowDimensions(width, height)
 
